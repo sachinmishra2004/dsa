96739339daacdc7904108003003b6a7e2cb0bb1b
Sachin mishra <119784607+sachinmishra2004@users.noreply.github.com>
2023-04-04 00:14:01 +0530

    Update successful pairs of spells ans potions.cpp

--- dsa/successful pairs of spells ans potions.cpp
+++ dsa/successful pairs of spells ans potions.cpp
@@ -21,3 +21,64 @@ public:
         return ans;
     }
 };
+
+appp == 2
+    
+class Solution {
+public:
+    
+    int BinarySearch(int l, int r, vector<int>& potions, int target) {
+        
+        //we have to find the index of first element greater than or equal to target (minPotion)
+        
+        int index = -1;
+        int mid = 0;
+        
+        while(l <= r) {
+            
+            mid = l + (r-l)/2;
+            
+            if(potions[mid] >= target) {
+                index = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+            
+        }
+        
+        return index;
+    }
+    
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int m = spells.size();
+        
+        int n = potions.size();
+        
+        sort(begin(potions), end(potions));
+        
+        int maxPotion = potions[n-1];
+        
+        vector<int> answer;
+        
+        
+        for(int i = 0; i<m; i++) {
+            
+            int spell = spells[i];
+            
+            long long minPotion = ceil((1.0*success)/spell);
+            
+            if(minPotion > maxPotion) {
+                answer.push_back(0);
+                continue;
+            }
+            
+            int index = BinarySearch(0, n-1, potions, minPotion);//lower_bound(begin(potions), end(potions), minPotion) - begin(potions);
+            
+            answer.push_back(n-index);
+            
+        }
+        
+        return answer;
+    }
+};
