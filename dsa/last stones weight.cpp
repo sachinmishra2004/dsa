04a61564ccbc5e72dd8a8e03ff1422451bb46174
Sachin mishra <119784607+sachinmishra2004@users.noreply.github.com>
2023-04-25 00:37:45 +0530

    Add files via upload

---
+++ dsa/last stones weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+    priority_queue<int>pq;
+
+    for(int&stone: stones){
+        pq.push(stone);
+    }
+    while(pq.size()>1){
+        int x = pq.top();
+        pq.pop();
+        int y = pq.top();
+        pq.pop();
+        if(x!=y)
+        pq.push(abs(x-y));
+    }
+    return pq.top();
+   
+    }     
+};
