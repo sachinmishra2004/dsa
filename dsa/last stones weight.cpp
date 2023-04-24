37385cde11646daf65cef51df4b37dc2a2231837
Sachin mishra <119784607+sachinmishra2004@users.noreply.github.com>
2023-04-25 00:39:01 +0530

    Update last stones weight.cpp

--- dsa/last stones weight.cpp
+++ dsa/last stones weight.cpp
@@ -18,3 +18,21 @@ public:
    
     }     
 };
+
+##### 2
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size()>1)
+        {
+            sort(stones.begin(),stones.end());
+            
+            int a = stones.back(); stones.pop_back();
+            int b = stones.back(); stones.pop_back();
+
+            stones.push_back(abs(a-b));
+        }
+        
+        return stones[0];
+    }     
+};
