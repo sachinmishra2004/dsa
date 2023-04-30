d001197fcf883b5d7fb4925c95a323938dbea828
Sachin mishra <119784607+sachinmishra2004@users.noreply.github.com>
2023-04-30 23:58:33 +0530

    Update cycle detection direct graph using .cpp

--- dsa/cycle detection direct graph using .cpp
+++ dsa/cycle detection direct graph using .cpp
@@ -34,3 +34,28 @@ public:
         return result;
     }
 };
+### dsu##################
+    vector<int> parent;
+    vector<int> rank;
+    
+    int find(int x) {
+        if(parent[x] == x)
+            return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+ 
+    
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
