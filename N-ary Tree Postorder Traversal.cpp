4b7e87219ef15e4f5ba28f643b3883b6c1787d93
Sachin mishra <119784607+sachinmishra2004@users.noreply.github.com>
2024-08-26 10:39:31 +0530

    Add files via upload

---
+++ N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,42 @@
+N-ary Tree Postorder Traversal
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void dfs(Node *root, vector<int>&ans){
+        if(!root) return;
+        for(int i = 0; i<root->children.size(); i++){
+            dfs(root->children[i],ans);
+        }
+
+        ans.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        vector<int>ans;
+        if(!root) {};
+
+        dfs(root,ans);
+
+        return ans;
+    }
+};
